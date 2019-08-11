1317061d79bb863f726ad8538f4d3d9a405da9e7
Sharanramdb <53758737+Sharanramdb@users.noreply.github.com>
2019-08-11 16:39:26 +0530

    Update and rename mark_grade.c to sgpa.c

--- sgpa.c
+++ sgpa.c
@@ -5,14 +5,14 @@ int main()
 {
 	int mark_grd(int);	
 	struct student
-	{
-		char usn[20];
-		char name[20];
-		int mark[20];
-		int crd[20];
-	}s1;
-	int i;
-	float tot=0,tot_crd=0,cgpa=0;
+{
+	char usn[20];
+	char name[20];
+	int mark[20];
+	int crd[20];
+}s1;
+int i;
+float tot=0,tot_crd=0,sgpa=0;
 	clrscr();
 	printf("Enter the name of student : \n");
 	scanf("%s",s1.name);
@@ -29,30 +29,30 @@ int main()
 	for(i=0;i<5;i++)
 	printf("MARKS IN SUBJECT %d : %d\n",i+1,s1.mark[i]);
 	for(i=0;i<5;i++)
-	{ tot=tot+(mark_grd(s1.mark[i])*s1.crd[i]);
-	  tot_crd=tot_crd+s1.crd[i];
-	}
-	cgpa=tot/tot_crd;
-	printf("CGPA	: %.1f",cgpa);
+		{ tot=tot+(mark_grd(s1.mark[i])*s1.crd[i]);
+	  	tot_crd=tot_crd+s1.crd[i];
+		}
+	sgpa=tot/tot_crd;
+	printf("SGPA	: %.1f",sgpa);
 	getch();
-	return 0;
+return 0;
 }
 int mark_grd(int mark)
 {
-	if(mark>=90&&mark<=100)
-	return 10;
-	else if(mark<90&&mark>=75)
-	return 9;
-	else if(mark<75&&mark>=60)
-	return 8;
-	else if(mark<60&&mark>=50)
-	return 7;
-	else if(mark<50&&mark>=45)
-	return 5;
-	else if(mark<45&&mark>=40)
-	return 4;
-	else if(mark<40)
-	return 0;
+if(mark>=90&&mark<=100)
+return 10;
+else if(mark<90&&mark>=75)
+return 9;
+else if(mark<75&&mark>=60)
+return 8;
+else if(mark<60&&mark>=50)
+return 7;
+else if(mark<50&&mark>=45)
+return 5;
+else if(mark<45&&mark>=40)
+return 4;
+else if(mark<40)
+return 0;
 }
 
 
